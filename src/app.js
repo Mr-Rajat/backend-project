3328756fcf5223db4a9a87249bffac699f8a4724
Rajat <rajatch133@gmail.com>
2024-01-06 00:01:50 +0530

    All setup configure

--- src/app.js
+++ src/app.js
@@ -13,11 +13,14 @@ app.use(cors({
 app.use(express.json({
     limit: '20kb' // Maximum request body size.
 }))
-
-app.use(express.urlencoded({extended: true, limit: "20kb"}))
-
+app.use(express.urlencoded({ extended: true, limit: "20kb" }))
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+// routes import
+
+import userRouter from './routes/user.routes.js'
+
+app.use('/api/v1/users', userRouter)
 
 export { app }
